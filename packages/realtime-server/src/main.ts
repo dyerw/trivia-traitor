15a72cf6b756660f88f97085d8d153a3b8ede78b
liam-dyer <liam.dyer@getcruise.com>
2023-08-16 11:26:05 -0700

    Setup websocket server

--- packages/realtime-server/src/main.ts
+++ packages/realtime-server/src/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
