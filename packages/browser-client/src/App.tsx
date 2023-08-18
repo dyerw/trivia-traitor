a36fa10d1b21867a7f6aa79401b519d3f0671a09
dave-hammarstedt <daveh@stardust.gg>
2023-08-17 17:33:11 -0700

    jury rig session ids in there

--- packages/browser-client/src/App.tsx
+++ packages/browser-client/src/App.tsx
@@ -1,10 +1,16 @@
 import { lazy } from 'solid-js';
 import { Routes, Route } from '@solidjs/router';
+import { sessionClient } from './utils/trpc';
+import { sessionIdCreated } from './store';
 
 const Home = lazy(() => import('./pages/Home'));
 const Lobby = lazy(() => import('./pages/Lobby'));
 
 function App() {
+  (async () => {
+    const { sessionId } = await sessionClient.sessionCreate.mutate();
+    sessionIdCreated(sessionId);
+  })();
   return (
     <>
       <Routes>
--- packages/realtime-server/src/main.ts
+++ packages/realtime-server/src/main.ts
@@ -3,21 +3,27 @@ import { router, publicProcedure } from './trpc';
 import { observable } from '@trpc/server/observable';
 import ws from 'ws';
 import { applyWSSHandler } from '@trpc/server/adapters/ws';
-
 import {
   createLobby,
   joinLobby,
   subscribeToLobbyChanged,
   Lobby,
   startGame,
+  sessionStore,
+  isLobbyOwner,
 } from './lobbies-subject';
+
 import { tap } from 'rxjs';
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import cors from 'cors';
+import { v4 as uuidV4 } from 'uuid';
+import { TRPCError } from '@trpc/server';
 
 const appRouter = router({
   lobbyCreate: publicProcedure
-    .input(z.object({ nickname: z.string() }))
+    .input(z.object({ nickname: z.string(), sessionId: z.string().uuid() }))
     .mutation(async (opts) => {
-      return createLobby(opts.input.nickname);
+      return createLobby(opts.input.nickname, opts.input.sessionId);
     }),
   lobbyJoin: publicProcedure
     .input(z.object({ nickname: z.string(), code: z.string() }))
@@ -37,11 +43,30 @@ const appRouter = router({
       });
     }),
   gameStart: publicProcedure
-    .input(z.object({ code: z.string() }))
+    .input(z.object({ code: z.string(), sessionId: z.string().uuid() }))
     .mutation((opts) => {
+      if (!isLobbyOwner(opts.input.code, opts.input.sessionId)) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+      }
       return startGame(opts.input.code);
     }),
 });
+
+const sessionRouter = router({
+  sessionCreate: publicProcedure.mutation(() => {
+    const sessionId = uuidV4();
+    sessionStore[sessionId] = {}; //TODO do we need an actual object
+    return {
+      sessionId,
+    };
+  }),
+});
+
+export type SessionRouter = typeof sessionRouter;
+createHTTPServer({
+  middleware: cors(),
+  router: sessionRouter,
+}).listen(3002);
 
 export type AppRouter = typeof appRouter;
 
