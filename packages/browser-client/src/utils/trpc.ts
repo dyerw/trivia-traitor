3e5616deec13d98046b62a5e92b33e15332ba1e4
liam-dyer <liam.dyer@getcruise.com>
2023-08-24 17:03:33 -0700

    setup env vars for client hsot

--- packages/browser-client/src/utils/trpc.ts
+++ packages/browser-client/src/utils/trpc.ts
@@ -3,8 +3,10 @@ import { createTRPCProxyClient, createWSClient, wsLink } from '@trpc/client';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import type { AppRouter } from '@trivia-traitor/realtime-server';
 
+const host = process.env.HOST ?? 'localhost';
+
 const wsClient = createWSClient({
-  url: `ws://localhost:3001`,
+  url: `ws://${host}:3001`,
 });
 
 export const client = createTRPCProxyClient<AppRouter>({
