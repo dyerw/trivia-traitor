6c6bee7e7bc229c22fb1eea77bd1219ded1a3c35
liam-dyer <liam.dyer@getcruise.com>
2023-09-06 16:33:34 -0700

    Complete rename

--- packages/browser-client/src/app/app.spec.tsx
+++ packages/browser-client/src/app/app.spec.tsx
@@ -20,6 +20,6 @@ describe('App', () => {
         <App />
       </BrowserRouter>
     );
-    expect(getByText(/Welcome browser-client-react/gi)).toBeTruthy();
+    expect(getByText(/Welcome browser-client/gi)).toBeTruthy();
   });
 });
--- packages/browser-client/vite.config.ts
+++ packages/browser-client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/browser-client-react',
+  cacheDir: '../../node_modules/.vite/browser-client',
 
   server: {
     port: 4200,
